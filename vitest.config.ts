93709fb2836d6577a5394f3c95f0c83752eb7713
thoroc <thomas.a.roche@gmail.com>
2024-08-07 20:28:29 +0100

    output file for test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     reporters: ['default', 'json'],
+    outputFile: 'converage/coverage-summary.json',
     coverage: { enabled: true, provider: 'istanbul' },
     disableConsoleIntercept: true,
   },
