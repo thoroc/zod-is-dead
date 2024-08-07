818e396a7a0edabcffb3beeb92416f8732257590
thoroc <thomas.a.roche@gmail.com>
2024-08-07 20:34:45 +0100

    splitting the coverage from the test in pipeline

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,18 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    reporters: ['default', 'json'],
+    reporters: ['default', 'json-summary', 'json'],
     outputFile: 'converage/coverage-summary.json',
-    coverage: { enabled: true, provider: 'istanbul' },
+    coverage: {
+      enabled: true,
+      provider: 'istanbul',
+      thresholds: {
+        lines: 60,
+        branches: 60,
+        functions: 60,
+        statements: 60,
+      },
+    },
     disableConsoleIntercept: true,
   },
 });
