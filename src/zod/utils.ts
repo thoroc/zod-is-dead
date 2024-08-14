25bb10b957b54c72e24726af09e74513a12f7a43
thoroc <thomas.a.roche@gmail.com>
2024-08-14 20:09:21 +0100

    forgot the documentation on new function

--- src/zod/utils.ts
+++ src/zod/utils.ts
@@ -1,3 +1,10 @@
+/**
+ * Retrieves the values of an enum-like object.
+ *
+ * @template T - The type of the enum-like object.
+ * @param obj - The enum-like object.
+ * @returns An array containing the values of the enum-like object.
+ */
 export const getEnumValues = <T extends Record<string, unknown>>(obj: T) => {
   return Object.values(obj) as [(typeof obj)[keyof T]];
 };
